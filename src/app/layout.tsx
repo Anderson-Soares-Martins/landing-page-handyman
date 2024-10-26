2f413d8de06bd980eb2968871d7ca9bd2e5bf801
Anderson Soares Martins <78911291+Anderson-Soares-Martins@users.noreply.github.com>
2024-10-26 15:12:56 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,7 @@ export default function RootLayout({
               href={generateWhatsappLink(
                 "Opa amigo, tudo bem? Gostaria de agendar um serviço!"
               )}
+passHref
             >
               <PulsatingButton>
                 <FaWhatsapp size={32} />
