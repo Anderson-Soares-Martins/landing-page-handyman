11523a696491af749e15ee29247f8dc6ac7d943f
Anderson-Soares-Martins <andersoaresmartins@gmail.com>
2024-10-29 12:28:49 -0300

    ajuste

--- src/utils.ts
+++ src/utils.ts
@@ -8,10 +8,11 @@
   const phoneNumber = "5548999652972";
 
   export const  generateWhatsappLink = (message: string) => {
-    if (typeof window !== 'undefined') {
+    if (typeof window !== 'undefined' && typeof window.gtag === 'function') {
       window.gtag('event', 'conversion', {
         'send_to': 'AW-16755974704/mkfGCI7Y7eIZELDE77U-',
       });
+      console.log('Event sent to Google Ads');
     }
     const encodedMessage = encodeURIComponent(message);
     return `https://wa.me/${phoneNumber}/?text=${encodedMessage}`;
